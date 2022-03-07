d87bffcb844a69cd59d5b7b93ab636f615cb5a65
Zihang He <rylaihzh@gmail.com>
2022-03-07 14:52:41 +0800

    reformat

--- db/01-init.sh
+++ db/01-init.sh
@@ -7,10 +7,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   \connect $APP_DB_NAME $POSTGRES_USER
   BEGIN;
     CREATE TABLE IF NOT EXISTS guests (
-        id BIGSERIAL NOT NULL PRIMARY KEY,                                                                                                                                                
-        name VARCHAR(50) NOT NULL,                                                                                                                                                        
-        phone TEXT,                                                                                                                                                                       
-        extras INTEGER,                                                                                                                                                                  
+        id BIGSERIAL NOT NULL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        phone TEXT,
+        extras INTEGER,
         need_accommodation BOOLEAN
 	);
   COMMIT;
